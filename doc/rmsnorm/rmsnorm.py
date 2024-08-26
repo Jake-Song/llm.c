031ab9e03c8fe4bb8b93757ca374c384b50e2974
jake <mubok0212@gmail.com>
2024-08-27 00:21:25 +0900

    fix typo

--- doc/rmsnorm/rmsnorm.py
+++ doc/rmsnorm/rmsnorm.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 eps = 1e-6
 
@@ -7,7 +8,7 @@ class RMSNorm:
     @staticmethod
     def forward(x, w, b: float = 0.0):
         B, T, C = x.size()
-        x2 = x.pow(2) # B,T,C
+        x2 = x ** 2 # B,T,C
         x2_sum = x2.sum(-1, keepdim=True) # B,T,1
         mean = x2_sum / C # B,T,1
         rsqrt = (mean + eps) ** -0.5 # B,T,1
@@ -26,16 +27,17 @@ class RMSNorm:
         db = dout.sum((0, 1))
         dw = (dout * norm).sum((0, 1))
         # gradients for input
-        dnorm = dout * w
-        dx = dnorm * rsqrt
+        dnorm = dout * w # B,T,C
+        dx = dnorm * rsqrt # B,T,C
         dx = dx.clone()
-        drsqrt = (dnorm * x).sum(-1, keepdim=True)
-        dmean = drsqrt * -0.5 * (mean + eps) ** -1.5
-        dx2_sum = dmean * C ** -1
-        dx2 = dx2_sum * torch.ones_like(x)
-        dx += dx2 * (2 * x)  
+        drsqrt = (dnorm * x).sum(-1, keepdim=True) # B,T,1
+        dmean = drsqrt * -0.5 * (mean + eps) ** -1.5 # B,T,1
+        dx2_sum = dmean * C ** -1 # B,T,1
+        dx2 = dx2_sum * torch.ones_like(x) # B,T,C
+        dx += dx2 * (2 * x) # B,T,C     
          
         return dx, dw, db
+
 
 # create a small dummy example and check w.r.t PyTorch backward
 B = 2
@@ -52,12 +54,13 @@ dx, dw, db = RMSNorm.backward(dout, cache)
 # compare to PyTorch autograd
 fakeloss = (out * dout).sum()
 fakeloss.backward()
+
 print("dx error:", (x.grad - dx).abs().max().item())
 print("dw error:", (w.grad - dw).abs().max().item())
 print("db error:", (b.grad - db).abs().max().item())
 
 # for reference checking in C also
-x, w, mean, rstd = cache
+x, w, mean, rsqrt = cache
 
 def write(tensor, handle):
     handle.write(tensor.detach().numpy().astype("float32").tobytes())
@@ -69,7 +72,7 @@ with open('ln.bin', 'wb') as file:
     write(b, file) # (C, )
     write(out, file) # (B, T, C)
     write(mean, file) # (B, T)
-    write(rstd, file) # (B, T)
+    write(rsqrt, file) # (B, T)
     write(dout, file) # (B, T, C)
     write(dx, file) # (B, T, C)
     write(dw, file) # (C, )
